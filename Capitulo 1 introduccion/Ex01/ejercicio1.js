63967053d97d3a83004364b12979cb697be12e77
antonirojasx <gonzlesrojasa79@gmail.com>
2026-05-28 20:14:08 -0500

    version one

---
+++ Capitulo 1 introduccion/Ex01/ejercicio1.js
@@ -0,0 +1,10 @@
+/** Este codigo nos permite realizar comentarios en el codigo, es decir, para escribir texto que no será ejecutado por el navegador */
+
+// Este es un comentario de una sola línea
+
+/* Este es un comentario de varias líneas
+   que puede abarcar múltiples líneas de código */
+
+document.write("Hola, este es un comentario de una sola línea"); // Este comentario explica que se está escribiendo un mensaje en la página web
+
+console.log("Este es un comentario de varias líneas"); /* Este comentario explica que se está escribiendo un mensaje en la consola del navegador */
